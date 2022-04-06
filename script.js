9780a3de87c1bfd73fa96bcb1ad17ef98878f3a1
lyes07 <83643064+lyes07@users.noreply.github.com>
2022-04-06 12:55:06 +0100

    home page and fixing the links

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ function hidemenu(){
 function showmenu(){
     navLinks.style.right = "0";
 }
+
+
+const course1 = document.getElementById("c1");
+course1.addEventListener("click",toCourse1);
+function toCourse1(){
+    window.location.href = "course.html";
+}
+
+const course2 = document.getElementById("c2");
+course2.addEventListener("click",toCourse2);
+function toCourse2(){
+    window.location.href = "course.html";
+}
+
+const course3 = document.getElementById("c3");
+course3.addEventListener("click",toCourse3);
+function toCourse3(){
+    window.location.href = "course.html";
+}
