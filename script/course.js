1e435c5d0b46d4c404b18339ec7b3d0a688b4ab5
lyes07 <83643064+lyes07@users.noreply.github.com>
2022-05-03 22:20:56 +0100

    refactering the code

--- script/course.js
+++ script/course.js
@@ -44,23 +44,23 @@ cc3.addEventListener("click",e => {
 ////////// conjonction /////////
 
 const conjonction = ["0","0","0","1"];
-const model = document.querySelector("#question1");
-const open = document.querySelector(".open-button");
-const close = document.querySelector(".close-button");
-const chech = document.querySelector(".check-button");
+const model1 = document.querySelector("#question1");
+const open1 = document.querySelector(".open-button1");
+const close1 = document.querySelector(".close-button1");
+const check1 = document.querySelector(".check-button1");
 
-chech.addEventListener("click",()=>{
+check1.addEventListener("click",()=>{
     if(q1r1.innerHTML==conjonction[0] && q1r2.innerHTML==conjonction[1] && q1r3.innerHTML==conjonction[2] && q1r4.innerHTML==conjonction[3]){
         alert("You are right!! Congratulation");
-        model.close();
+        model1.close();
     }
     else{
         alert("You are wrong!! Try aging!!");
     }
 });
 
-open.addEventListener('click', ()=>{
-    model.showModal();
+open1.addEventListener('click', ()=>{
+    model1.showModal();
     const q1r1 = document.getElementById("q1r1");
     const q1r2 = document.getElementById("q1r2");
     const q1r3 = document.getElementById("q1r3");
@@ -100,12 +100,71 @@ open.addEventListener('click', ()=>{
     });
 });
 
-close.addEventListener('click', ()=>{
-    model.close();
+close1.addEventListener('click', ()=>{
+    model1.close();
 });
 
 
-////////// conjonction /////////
+////////// Disjonction /////////
 
+const disjonction = ["0","1","1","1"];
+const model2 = document.querySelector("#question2");
+const open2 = document.querySelector(".open-button2");
+const close2 = document.querySelector(".close-button2");
+const check2 = document.querySelector(".check-button2");
 
+check2.addEventListener("click",()=>{
+    if(q2r1.innerHTML==disjonction[0] && q2r2.innerHTML==disjonction[1] && q2r3.innerHTML==disjonction[2] && q2r4.innerHTML==disjonction[3]){
+        alert("You are right!! Congratulation");
+        model2.close();
+    }
+    else{
+        alert("You are wrong!! Try aging!!");
+    }
+});
+
+open2.addEventListener('click', ()=>{
+    model2.showModal();
+    const q2r1 = document.getElementById("q2r1");
+    const q2r2 = document.getElementById("q2r2");
+    const q2r3 = document.getElementById("q2r3");
+    const q2r4 = document.getElementById("q2r4");
+
+    q2r1.addEventListener("click", ()=>{
+            if(q2r1.innerHTML == "0"){
+                q2r1.innerHTML = "1";
+            }
+            else{
+                q2r1.innerHTML = "0";
+            }
+    });
+    q2r2.addEventListener("click", ()=>{
+            if(q2r2.innerHTML == "0"){
+                q2r2.innerHTML = "1";
+            }
+            else{
+                q2r2.innerHTML = "0";
+            }
+    });
+    q2r3.addEventListener("click", ()=>{
+            if(q2r3.innerHTML == "0"){
+                q2r3.innerHTML = "1";
+            }
+            else{
+                q2r3.innerHTML = "0";
+            }
+    });
+    q2r4.addEventListener("click", ()=>{
+            if(q2r4.innerHTML == "0"){
+                q2r4.innerHTML = "1";
+            }
+            else{
+                q2r4.innerHTML = "0";
+            }
+    });
+});
+
+close2.addEventListener('click', ()=>{
+    model2.close();
+});
 
