94e2e1643cee8084d2c03a4aeea46417faa528fd
lyes07 <83643064+lyes07@users.noreply.github.com>
2022-06-18 23:30:53 +0100

    quiz done

--- backend/q.php
+++ backend/q.php
@@ -4,7 +4,18 @@ if(isset($_POST["chapter"])){
     $conn = mysqli_connect("localhost","root","","strm");
     $data = array();
     $input =$_POST["chapter"];
-    $query="SELECT * FROM questions WHERE qst_type='multichoice';";
+    if($input == "0"){
+        $query="SELECT * FROM questions WHERE qst_type='multichoice';";
+    }
+    if($input == "1"){
+        $query="SELECT * FROM questions WHERE qst_type='multichoice' AND chapter = '1';";
+    }
+    if($input == "2"){
+        $query="SELECT * FROM questions WHERE qst_type='multichoice' AND chapter = '2';";
+    }
+    if($input == "3"){
+        $query="SELECT * FROM questions WHERE qst_type='multichoice' AND chapter = '3';";
+    }
 
     $result = $conn->query($query);
 
--- script/q.js
+++ script/q.js
@@ -1,3 +1,122 @@
+var qst
+var q = [[]]
+var numberOfQuestions
+let trys = []
+
+function start(){
+    questionIndex.innerHTML = "Question "+i+" of "+numberOfQuestions;
+    question.innerHTML = q[i-1]["qst_text"]
+    var temp =  q[i-1]["opt"].split(",")
+    for(let j=0; j<4; j++){
+        options[j].innerHTML = temp[j]
+    }
+}
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+function source(e){
+    document.querySelector(".center").style.display = "none"
+    document.querySelectorAll(".hide").forEach(element => {
+        element.style.visibility = "visible";
+    });
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST","../backend/q.php",true);
+    xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xhr.send('chapter='+e);
+    xhr.onload = function(){
+        if(this.status == 200){
+            qst = JSON.parse(this.responseText);
+            numberOfQuestions = qst.length
+            for(let j=0; j<numberOfQuestions; j++){
+                trys[j] = false
+            }
+            q=shuffle(qst)
+            start()
+            console.log(q)
+        }
+        else{
+            console.log("status not right")
+        }
+    }        
+}
+let i = 1
+let score = 0
+const questionIndex = document.querySelector(".question-number")
+const question = document.querySelector(".question-text")
+const options = document.querySelectorAll(".option")
+const quiz = document.querySelector(".quiz-box")
+
+
+
+function next(){
+    i++
+    if(i<=numberOfQuestions){
+        reset()
+        start()
+    }
+    else{
+        displayScore()
+    }
+}
+
+function check(e){
+    if(trys[i-1] == false){
+        if(e.innerHTML == q[i-1]["answer"]){
+            e.style.backgroundColor = "#00c52e";
+            trys[i-1] = true
+            score +=1
+        }
+        else{
+            e.style.backgroundColor = "#f0273b";
+            for(let j=0; j<4; j++){
+                if(options[j].innerHTML == q[i-1]["answer"]){
+                    options[j].style.backgroundColor = "#00c52e";
+                    trys[i-1] = true
+                }
+            }
+        }
+    }
+    else{
+
+    }
+}
+
+function reset(){
+    for(let j=0; j<4; j++){
+        options[j].style.backgroundColor = "#8d3d6f";
+    }
+}
+
+function displayScore(){
+    quiz.innerHTML = "<h1 style='margin:40px auto;text-align: center;'>Your Score is : "+score+" / "+numberOfQuestions+"</h1>"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+
 var qJason = returnQuestions()
 function init(){
     if(qJason){
@@ -50,6 +169,7 @@ document.onload = ()=>{
 }
 console.log(q) */
 
+/*
 init()
 
 
@@ -106,3 +226,4 @@ function displayScore(){
 }
 
 
+ */
